47c9f2e5dfffd20d64d904684c1956de4337e076
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-12-20 12:23:07 +0530

    Add files via upload

---
+++ cardproject/weatherapp/projecttwo/src/Headinglogo.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+
+// const Headinglogo = ({logo , weatherAppName, titleheading}) => {
+//   return (
+//     <div>
+      
+//       <div className="weather-app-logo">
+//         {logo ? <img src={logo} alt="logo"/> : weatherAppName[0]}
+//         <h4>{weatherAppName}</h4>
+//         <img src="images/icon-units.svg" alt="unit-logo" />
+//       </div>
+
+//       <div className="title-heading">
+//         <h2>{titleheading}</h2>
+//       </div>
+
+//       <div className="search-bar">
+//         <div class="input-wrapper">
+//             <img src="images/icon-search.svg" alt="search-logo" />
+//             <input type="text" placeholder="Search for a place..." />
+//         </div>
+//         <button class="search-btn">Search</button>
+//       </div>
+
+
+        
+//     </div>
+//   )
+// }
+
+// export default Headinglogo
